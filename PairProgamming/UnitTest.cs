1e4b138c4224aa71fbd55760a98fc52d6e0c515e
Zabring Antry <zaa0146@utulsa.edu>
2018-10-10 13:41:49 -0500

    SetText and sendButton_Click unit test
    Use for both client and server??

--- PairProgamming/UnitTest.cs
+++ PairProgamming/UnitTest.cs
@@ -1,9 +1,8 @@
 using System;
 using System.Collections.Generic;
 
-//Unit Test Examples 
-//Assert ideas for our code
-//Not all applicable
+//Unit Test
+//For DoWork, sendButton_Click, SetText
 
 namespace WizardOfOz
 {
@@ -25,7 +24,38 @@ namespace WizardOfOz
                 button.Click();
 
             }
-   
+
+            [Test]
+            public void TestDoWork()
+            {
+                TDoWork controller = new TDoWork();
+                controller.Show();
+
+                TextBoxTester textBox = new TextBoxTester("txtName");
+                textBox["Text"] = "Hello Wizard";
+
+                ButtonTester button = new ButtonTester("sendButton_Click");
+                button.Click();
+
+            }
+
+            [TestCase(new byte[] { 0x00, 0x01, 0x02, 0x03 })]
+            public void sample(byte[] input)
+            {
+                Assert.That(input, Is.EqualTo(new byte[] { 0x00, 0x01, 0x02, 0x03 }));
+            }
+
+            [Test]
+            public void SimpleTestUsingMessageBox()
+            {
+ 
+            WhenCalled(() => MessageBox.Show(String.Empty)).WillReturn(DialogResult.OK);
+
+            MessageBox.Show("This is a message");
+
+            Verify.WasCalledWithExactArguments(() => MessageBox.Show("This is a message"));
+            }
+
         }
 
     }
