f281e05087ef9a7f0cd10c4c2c238972958e4f9a
Peter Robe <probe191919@gmail.com>
2018-10-10 22:40:40 -0500

    Added one way video streaming via ffmpeg. If this doesn't work for you, please tell me. Debug mode is on, so there's 6 windows lol

--- WizardOfOzClient/Form1.cs
+++ WizardOfOzClient/Form1.cs
@@ -9,6 +9,10 @@ using System.Threading;
 using System.Windows.Forms;
 using System.Net.Sockets;
 using System.Speech.Synthesis;
+using AForge.Video;
+using AForge.Video.DirectShow;
+using System.Diagnostics;
+using System.IO;
 
 namespace WizardOfOzClient
 {
@@ -26,6 +30,7 @@ namespace WizardOfOzClient
         public Form1()
         {
             InitializeComponent();
+            InitComboBoxes();
             reader = new SpeechSynthesizer();
             client = new TcpClient(hostName, portNumber);
             ns = client.GetStream();
@@ -79,6 +84,168 @@ namespace WizardOfOzClient
                     this.allMessagesBox.AppendText(text + "\r\n");
                 }
             }
+        }
+
+        private void InitComboBoxes()
+        {
+            InitComboBox1();
+            InitComboBox2();
+        }
+
+        /// <summary> 
+        ///  Initializes the webcam dropdown menu. Populates it with the avaliable webcams on your system. If you pick one that doesn't work, it will fail to launch the video.
+        /// </summary> 
+        private void InitComboBox1()
+        {
+            bool DeviceExist = false;
+            FilterInfoCollection videoDevices;
+            //VideoCaptureDevice videoSource = null;
+
+            try
+            {
+                videoDevices = new FilterInfoCollection(FilterCategory.VideoInputDevice);
+                comboBox1.Items.Clear();
+                if (videoDevices.Count == 0)
+                    throw new ApplicationException();
+
+                DeviceExist = true;
+                foreach (FilterInfo device in videoDevices)
+                {
+                    comboBox1.Items.Add(device.Name);
+                }
+                comboBox1.SelectedIndex = 0;
+            }
+            catch (ApplicationException)
+            {
+                DeviceExist = false;
+                comboBox1.Items.Add("No capture device on your system");
+            }
+        }
+
+        /// <summary> 
+        ///  Initializes the audio dropdown menu. Populates it with the avaliable webcams on your system. 
+        /// </summary> 
+        private void InitComboBox2()
+        {
+            bool DeviceExist = false;
+            FilterInfoCollection audioDevices;
+
+            try
+            {
+                audioDevices = new FilterInfoCollection(FilterCategory.AudioInputDevice);
+                comboBox2.Items.Clear();
+                if (audioDevices.Count == 0)
+                    throw new ApplicationException();
+
+                DeviceExist = true;
+                foreach (FilterInfo device in audioDevices)
+                {
+                    comboBox2.Items.Add(device.Name);
+                }
+                comboBox2.SelectedIndex = 0;
+            }
+            catch (ApplicationException)
+            {
+                DeviceExist = false;
+                comboBox2.Items.Add("No capture device on your system");
+            }
+        }
+
+        private Process videoProcess; //keep track of the process so we can kill it later
+        private Process audioProcess;
+        private bool videoProcessFirstStarted = false; //used to store whether or not the process has been started before. because if that's the case, it's null.
+        private bool audioProcessFirstStarted = false;
+
+        /// <summary> 
+        ///  "Start Stream" button. Checks if the streams are already running before starting either of them.
+        /// </summary> 
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (comboBox1.SelectedItem != null && comboBox2.SelectedItem != null)
+            {
+                if (!videoProcessFirstStarted || videoProcess.HasExited)
+                {
+                    startFFmpeg("video", comboBox1.Text);
+                }
+                if (!audioProcessFirstStarted || audioProcess.HasExited)
+                {
+                    startFFmpeg("audio", comboBox2.Text);
+                }
+            }
+            else
+            {
+                allMessagesBox.AppendText("Select input devices!!!\r\n");
+            }
+        }
+
+
+        /// <summary> 
+        ///  Depending on the mediaType (either "video" or "audio), ffmpeg launches either stream. Some comments below to runs them without a window. Pretty slick.
+        /// </summary> 
+        private void startFFmpeg(String mediaType, String hardwareName)
+        {
+            Process process = new Process();
+            if (mediaType == "video")
+            {
+                videoProcess = new Process();
+                process = videoProcess;
+                process.StartInfo.Arguments = "-f dshow -video_size 640x480 -r 30 -i video=\"" + hardwareName + "\" -filter:v \"setpts=(39/40)*PTS\" -vcodec libx264 -preset ultrafast -f mpegts udp://127.0.0.1:1234";
+                                                                            //the is the video encoder/streamer. You can edit the udp address. 
+                videoProcessFirstStarted = true;
+            }
+            else if (mediaType == "audio")
+            {
+                audioProcess = new Process();
+                process = audioProcess;
+                process.StartInfo.Arguments = "-f dshow -i audio=\"" + hardwareName + "\" -af asetrate=44100*(20/19),aresample=44100 -acodec aac -f mpegts udp://127.0.0.1:1235 ";
+                                                                            //this is the audio encoder/streamer. You can edit the address here. 
+                audioProcessFirstStarted = true;
+
+            }
+            else
+            {
+                Console.WriteLine("Invalid media name");
+                return;
+            }
+            process.StartInfo.FileName = Path.GetDirectoryName(System.Diagnostics.Process.GetCurrentProcess().MainModule.FileName) +
+                                    @"\..\..\..\Libraries\ffmpeg.exe";
+            //process.StartInfo.UseShellExecute = false; //these lines let you run it without a window. disable for testing.
+            //process.StartInfo.CreateNoWindow = true;
+            process.Start();
+        }
+
+        /// <summary> 
+        ///  Tries to kill both ffmpeg processes. Checks if they are actually running first.
+        /// </summary> 
+        private void closeStream()
+        {
+            if (audioProcess != null && !audioProcess.HasExited)
+            {
+                audioProcess.Kill();
+            }
+            if (videoProcess != null && !videoProcess.HasExited)
+            {
+                videoProcess.Kill();
+            }
+        }
+
+
+        /// <summary> 
+        ///  "Stop Stream" button
+        /// </summary> 
+        private void button2_Click(object sender, EventArgs e)
+        {
+            closeStream();
+        }
+
+        /// <summary> 
+        ///  We want to close the processes if they are still running before we close the form. It's not fun having invisible processes running without an easy way to close them.
+        /// </summary> 
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            Console.WriteLine("Closing Event Triggered");
+            closeStream();
+            Application.Exit();
         }
     }
 }
--- WizardOfOzServer/Form1.Designer.cs
+++ WizardOfOzServer/Form1.Designer.cs
@@ -34,34 +34,33 @@
             this.sendButton = new System.Windows.Forms.Button();
             this.allMessagesBox = new System.Windows.Forms.TextBox();
             this.label2 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // messageBox
             // 
-            this.messageBox.Location = new System.Drawing.Point(290, 804);
-            this.messageBox.Margin = new System.Windows.Forms.Padding(6, 6, 6, 6);
+            this.messageBox.Location = new System.Drawing.Point(145, 418);
             this.messageBox.Name = "messageBox";
-            this.messageBox.Size = new System.Drawing.Size(1112, 31);
+            this.messageBox.Size = new System.Drawing.Size(558, 20);
             this.messageBox.TabIndex = 0;
             // 
             // label1
             // 
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label1.Location = new System.Drawing.Point(20, 806);
-            this.label1.Margin = new System.Windows.Forms.Padding(6, 0, 6, 0);
+            this.label1.Location = new System.Drawing.Point(10, 419);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(243, 30);
+            this.label1.Size = new System.Drawing.Size(129, 16);
             this.label1.TabIndex = 1;
             this.label1.Text = "Enter message here";
             // 
             // sendButton
             // 
             this.sendButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.sendButton.Location = new System.Drawing.Point(1426, 800);
-            this.sendButton.Margin = new System.Windows.Forms.Padding(6, 6, 6, 6);
+            this.sendButton.Location = new System.Drawing.Point(713, 416);
             this.sendButton.Name = "sendButton";
-            this.sendButton.Size = new System.Drawing.Size(150, 44);
+            this.sendButton.Size = new System.Drawing.Size(75, 23);
             this.sendButton.TabIndex = 2;
             this.sendButton.Text = "Send";
             this.sendButton.UseVisualStyleBackColor = true;
@@ -69,38 +68,57 @@
             // 
             // allMessagesBox
             // 
-            this.allMessagesBox.Location = new System.Drawing.Point(26, 69);
-            this.allMessagesBox.Margin = new System.Windows.Forms.Padding(6, 6, 6, 6);
+            this.allMessagesBox.Location = new System.Drawing.Point(13, 93);
             this.allMessagesBox.Multiline = true;
             this.allMessagesBox.Name = "allMessagesBox";
-            this.allMessagesBox.Size = new System.Drawing.Size(1546, 696);
+            this.allMessagesBox.Size = new System.Drawing.Size(775, 307);
             this.allMessagesBox.TabIndex = 3;
-            this.allMessagesBox.TextChanged += new System.EventHandler(this.allMessagesBox_TextChanged);
             // 
             // label2
             // 
             this.label2.AutoSize = true;
             this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label2.Location = new System.Drawing.Point(698, 17);
-            this.label2.Margin = new System.Windows.Forms.Padding(6, 0, 6, 0);
+            this.label2.Location = new System.Drawing.Point(349, 9);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(192, 36);
+            this.label2.Size = new System.Drawing.Size(99, 18);
             this.label2.TabIndex = 4;
             this.label2.Text = "Wizard Of Oz";
             // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(13, 35);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(126, 23);
+            this.button1.TabIndex = 7;
+            this.button1.Text = "Start Stream Receiver";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click_1);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(13, 64);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(126, 23);
+            this.button2.TabIndex = 8;
+            this.button2.Text = "Stop Stream Receiver";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1600, 865);
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.allMessagesBox);
             this.Controls.Add(this.sendButton);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.messageBox);
-            this.Margin = new System.Windows.Forms.Padding(6, 6, 6, 6);
             this.Name = "Form1";
             this.Text = "Wizard of Oz (server)";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -114,6 +132,8 @@
         private System.Windows.Forms.Button sendButton;
         private System.Windows.Forms.TextBox allMessagesBox;
         private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
     }
 }
 
